3e9e8451f06ea3fa07dd4b703f4056c352a86f72
RomanAtanasiu <roman.atanasiu01@estudiant.upf.edu>
2024-06-03 13:31:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fridgefindsmobile/ShoppingList.kt
@@ -0,0 +1,27 @@
+package com.example.fridgefindsmobile
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class ShoppingList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_shopping_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val buttonMain: ImageButton = findViewById(R.id.home_main)
+        buttonMain.setOnClickListener {
+            val intent = Intent(this, PantallaInicio::class.java)
+            startActivity(intent)
+        }
+    }
+}
